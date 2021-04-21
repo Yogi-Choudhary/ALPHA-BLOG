6ad6151ad7c57945c0ee7da3e5b2c6f56e599650
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-21 14:38:01 +0530

    bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   has_many :articles
   # username must be present and unique, min 3 max 25
    validates :username, presence: true, 
@@ -11,4 +12,5 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }, 
                       length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
