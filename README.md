fd296e29ad397d0bc7f4536c0a76bd0f1c9180d6
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-21 11:01:54 +0530

    Add_User_Validations

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Things you may want to cover:
 * Database creation = SQLite3
 
 * (1) CRUD Operations 
+
+- Users - create users table and model
+        - add validations
+        * username must be present and unique, min 3 max 25
+        * email address must be present and unique, max 105
+        * email must be valid email format, check with email regex 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  # username must be present and unique, min 3 max 25
+   validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+  # email must be valid email format, check with email regex
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # email address must be present and unique, max 105
+  validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
