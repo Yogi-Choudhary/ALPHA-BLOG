e50c425a239494bac5de77638e81e3e0186f73d8
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-21 16:34:04 +0530

    add_users_create_action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
