b40120ceda50b717bd5b5de5efba85c92970b30a
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-24 12:33:00 +0530

    add_category_model_and_test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_074352) do
+ActiveRecord::Schema.define(version: 2021_04_24_065513) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_04_22_074352) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
