d10a07860cf1c65fe9b1cdaff7e5ad924e32e763
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-24 13:36:23 +0530

    complete_categories_create_action_and_functional_test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,19 @@
 class CategoriesController < ApplicationController
 
   def new
-
+    @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+  
   def index
 
   end
@@ -11,5 +21,11 @@ class CategoriesController < ApplicationController
   def show
 
   end
-  
+
+   private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end 
